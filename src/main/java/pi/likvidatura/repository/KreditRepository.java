5ebf2b55e573630c6c91bd84ee168e9fdac80ee0
Jelena Mihajlovic <starglitz47@gmail.com>
2022-01-15 19:37:26 +0100

    remove kredit

--- src/main/java/pi/likvidatura/repository/KreditRepository.java
+++
@@ -1,7 +0,0 @@
-package pi.likvidatura.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pi.likvidatura.domain.Kredit;
-
-public interface KreditRepository extends JpaRepository<Kredit, Long> {
-}
